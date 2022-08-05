5d721e06c177611a7e5ea9722947f8eda889d02b
kerus <daskerus0@gmail.com>
2022-08-05 11:00:10 +0300

    help me

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -6,9 +6,40 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{env('APP_URL')}}/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <a class="btn" href="#">Добавить автора</a>
+
+<section>
+        <table id="table">
+            <tr>
+            <th>Исполнитель</th>
+            <th><a id="add" href="#">Добавить</a></th>
+            </tr>
+
+            <!-- <tr>
+            <td>Пасош</td>
+            <td><a class="remove" href="#">Удалить</a></td>
+            </tr>
+
+            <tr>
+            <td>Настя Кош</td>
+            <td><a class="remove" href="#">Удалить</a></td>
+            </tr>
+
+            <tr>
+            <td>Гражданская оборона</td>
+            <td><a class="remove" href="#">Удалить</a></td>
+            </tr>
+
+            <tr>
+            <td>Kizaru</td>
+            <td><a class="remove" href="#">Удалить</a></td>
+            </tr> -->
+
+        </table>
+    </section>
+
 </body>
 <script src = '{{env('APP_URL')}}/js/main.js'></script>
 </html>
